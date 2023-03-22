679832f2c67da1babe8a8beb323f7cc5ddbe2895
Ben Pate <ben@pate.org>
2023-03-21 22:38:52 -0600

    Comments

--- datatypes.go
+++ datatypes.go
@@ -1,9 +1,16 @@
 package builder
 
+// DataTypeString is a constant that represents a string parameter
 const DataTypeString = "STRING"
 
+// DataTypeInt is a constant that represents an integer parameter
 const DataTypeInt = "INT"
 
+// DataTypeFloat is a constant that represents a 64-bit integer parameter
+const DataTypeInt64 = "INT64"
+
+// DataTypeFloat is a constant that represents a boolean parameter
 const DataTypeBool = "BOOL"
 
+// DataTypeFloat is a constant that represents a mongodb "ObjectId" parameter (24-byte hex string)
 const DataTypeObjectID = "OBJECTID"
